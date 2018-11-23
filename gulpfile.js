61ae3f727e4cf8e39b387f3cc474fe03bad06069
Arunava <learningdroidarunava@gmail.com>
2018-11-23 15:42:21 +0530

    feat: Style optimization

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
 
 gulp.task('styles', function() {
     gulp.src('sass/*.scss')
@@ -9,4 +11,18 @@ gulp.task('styles', function() {
             browsers: ['last 2 versions']
         }))
         .pipe(gulp.dest('./css'));
+});
+
+gulp.task('scripts', function() {
+	//gulp.src(['js/main.js', 'js/restaurant_info.js'])
+	gulp.src('js/*.js')
+		.pipe(concat('all.js'))
+		.pipe(gulp.dest('./js'));
+});
+
+gulp.task('scripts-dist', function() {
+	gulp.src(['js/main.js', 'js/restaurant_info.js'])
+		.pipe(concat('all.js'))
+		.pipe(uglify())
+		.pipe(gulp.dest('./js'));
 });
